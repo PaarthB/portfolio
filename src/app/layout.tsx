d11e0150527072638862b05f8a17881580d8dc10
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-04-21 12:03:56 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Made with ❤ - © 2025. All Rights Reserved.</span>
+        </div>
+      </footer>
     </html>
   );
 }
