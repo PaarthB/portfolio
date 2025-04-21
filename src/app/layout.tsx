ddfaaf349de422e07ec2a0b7365e36d0c8eb4b9a
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-04-21 19:32:53 +1000

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,13 +28,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-{/*         <footer className="mt-64 md:w-[1240px] mx-auto flex flex-col justify-between py-6 sm:flex-row bg-white rounded-lg shadow-sm dark:bg-gray-900">
-          <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-              <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-              <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Powered by Next.js, Made with ❤️ - © 2025. All Rights Reserved.</span>
-          </div>
-        </footer> */}
-      </body> 
+      <footer
+        className="fixed bottom-0 bg-zinc-50 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/50 lg:text-left w-full">
+        <div className="bg-black/5 p-6 text-center">
+          <span>© 2025 Copyright:</span>
+          <p className="font-semibold" >Made with ❤️ in <b className="font-extrabold underline">Next.js</b></p>
+        </div>
+      </footer>      
+    </body> 
     </html>
   );
 }
