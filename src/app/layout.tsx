d0cb203251fa187bcd640cfddc6eec7aabbc99ec
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-04-21 12:23:03 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,13 +28,13 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <footer className="mt-64 md:w-[1240px] mx-auto flex flex-col justify-between py-6 sm:flex-row bg-white rounded-lg shadow-sm dark:bg-gray-900">
+{/*         <footer className="mt-64 md:w-[1240px] mx-auto flex flex-col justify-between py-6 sm:flex-row bg-white rounded-lg shadow-sm dark:bg-gray-900">
           <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
               <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
               <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Powered by Next.js, Made with ❤️ - © 2025. All Rights Reserved.</span>
           </div>
         </footer>
-      </body>
+      </body> */}
     </html>
   );
 }
