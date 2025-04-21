e39611ede1027dabdc9c6d7ed3d8c442d69f6083
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-04-21 12:24:24 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
               <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
               <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Powered by Next.js, Made with ❤️ - © 2025. All Rights Reserved.</span>
           </div>
-        </footer>
-      </body> */}
+        </footer> */}
+      </body> 
     </html>
   );
 }
