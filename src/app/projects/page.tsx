2f7b9b1ab5ac32fa4d1b27bcb20cb9649bdacfbc
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-04-23 07:34:19 +1000

    fix projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "../components/navbar"
 
-export default function Experience() {
+export default function Projects() {
     return (
         <main className="bg-gray-50 h-screen">
             <Navbar />
@@ -16,9 +16,12 @@ export default function Experience() {
                                 <path fill="#ff9a00" d="M116 140H78.8l-7.6 23.5c-.2.9-1 1.5-1.9 1.4H50.5c-1.1 0-1.4-.6-1.1-1.8l32.2-92.7c.3-1 .6-2.1 1-3.3.4-2.1.6-4.3.6-6.5-.1-.5.3-1 .8-1.1h25.9c.8 0 1.2.3 1.3.8l36.5 103c.3 1.1 0 1.6-1 1.6h-20.9c-.7.1-1.4-.4-1.6-1.1L116 140zm-31.4-20.3H110c-.6-2.1-1.4-4.6-2.3-7.2-.9-2.7-1.8-5.6-2.7-8.6-1-3.1-1.9-6.1-2.9-9.2s-1.9-6-2.7-8.9c-.8-2.8-1.5-5.4-2.2-7.8H97c-.9 4.3-2 8.6-3.4 12.9-1.5 4.8-3 9.8-4.6 14.8-1.4 5.1-2.9 9.8-4.4 14zM170 77c-3.3.1-6.5-1.2-8.9-3.5-2.3-2.5-3.5-5.8-3.4-9.2-.1-3.4 1.2-6.6 3.6-8.9s5.6-3.5 8.9-3.5c3.9 0 6.9 1.2 9.1 3.5 2.2 2.4 3.4 5.6 3.3 8.9.1 3.4-1.1 6.7-3.5 9.2-2.3 2.4-5.7 3.7-9.1 3.5zm-11.2 86.8v-77c0-1 .4-1.4 1.3-1.4h19.8c.9 0 1.3.5 1.3 1.4v77c0 1.1-.4 1.6-1.3 1.6h-19.6c-1 0-1.5-.6-1.5-1.6z"/>
                             </svg>
                         </span>
-                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Founding Engineer - MapAI <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300 ms-3"><b>Latest</b></span></h3>
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Founding Engineer - MapAI</h3>
                         <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Apr 2024 - Jun 2024</time>
-                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Full stack application design with best-practices for geo-spatial analysis tools working with Data Scientists, Researchers & Business Leaders. Tools used include <b>LangChain, OpenAI LLMs, Python, TypeScript, Tailwind CSS and Docker</b></p>
+                        <ul className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 gap-y-4 list-disc">                    
+                            <li className="pb-1.5"><u>Project funded by NSW Government</u></li>
+                            <li className="pb-1.5">Full stack application design with best-practices for geo-spatial analysis tools working with Data Scientists, Researchers & Business Leaders. Tools used include <b>LangChain, OpenAI LLMs, Python, TypeScript, Tailwind CSS and Docker</b></li>
+                        </ul>
                         <a href="https://mapai.net/about" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
@@ -41,7 +44,10 @@ export default function Experience() {
                         </span>
                         <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">LockR - a PyPi Library</h3>
                         <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on May 21, 2022</time>
-                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Open source CLI tool which solves the problem of concurrency management for distributed applications in cloud, using <b>Redis</b>.</p>
+                        <ul className="list-disc mb-4 text-base font-normal text-gray-500 dark:text-gray-400 gap-y-4">                    
+                            <li className="pb-1.5"><u>Used by by both research students and big tech companies</u></li>
+                            <li className="pb-1.5">Open source CLI tool which solves the problem of concurrency management for distributed applications in cloud, using <b>Redis</b>.</li>
+                        </ul> 
                         <a href="https://github.com/PaarthB/LockR" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700" target="_blank">
                             <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd"/>
@@ -50,7 +56,6 @@ export default function Experience() {
                     </li>
                 </ol>
             </section>
-
         </main>
     )
 }
