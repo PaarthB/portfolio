4962e99d117d6b0ba79eb7e582c432c38cf5e45b
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-04-22 22:45:26 +1000

    css fixes

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../components/navbar";
 
 export default function Experience() {
   return (
-    <main className="bg-blue-50 h-full">
+    <main>
       <Navbar />
       <section className='p-20 font-black text-black bg-repeat bg-cover bg-bottom w-full h-screen'>
         <h1 className="mb-8 text-2xl font-semibold tracking-tighter flex px-2">
@@ -22,83 +22,80 @@ export default function Experience() {
               </a>
             </div>
         </h1>
-
         <ol className="relative border-s border-gray-200 dark:border-gray-700">                  
-        <li className="mb-10 ms-6">            
-            <span className="absolute flex items-center justify-center w-6 h-6 bg-white rounded-full -start-3 ring-8 ring-white">
-            <svg className="fill-[#6fe3d0]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 228.35 105.4">
-                {/* <defs>
-                <style>.cls-1{fill:#6fe3d0;}
-                </style>
-                </defs> */}
-                <g id="Layer_2" data-name="Layer 2">
-                <g id="Layer_1-2" data-name="Layer 1">
-                    <path className="cls-1" d="M89.89,90a52.7,52.7,0,1,1,0-74.53l.13.12a9.16,9.16,0,0,1-.1,12.91h0l-30.7,30.7a9.2,9.2,0,0,1-13,0h0a9.2,9.2,0,0,1,0-13l23.29-23.3h0a34.29,34.29,0,1,0,7.37,54.1h0l61.57-61.51h0a52.7,52.7,0,1,1,0,74.52l-.06-.06a9.25,9.25,0,0,1,.09-13l30.65-30.64a9.2,9.2,0,0,1,13,0h0a9.2,9.2,0,0,1,0,13L158.84,82.56h0a34.29,34.29,0,1,0-7.38-54.1l0,0L89.9,90Z"/>
-                </g>
-                </g>
-            </svg>
-            </span>
-            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900">Senior Full Stack Engineer<span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300 ms-3"><b>Latest</b></span></h3>
-            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-black italic"><b>Evermed</b> - May, 2024 - Present</time>
-            <div className="mb-4 text-base font-normal text-gray-500 content-between gap-y-4">
-                <ul className="list-disc">                    
-                <li className="pb-1.5">
-                    Rollout of new e-commerce web & back-end features, optimizing 
-                    existing features, reducing tech-debt and improving operations 
-                    in a fast-paced environment., with lots of pair-programming.
-                </li>
-                <li className="pb-1.5">
-                    Working with <b>AWS, Python, C#/.NET, CSS, Node.js, React.js, Nginx</b> for{" "}
-                    <i>full-stack application development</i>. Introduced test based approach 
-                    cloud-infra and front-end applications, outlining testing strategy
-                </li>
-                </ul>
-            </div>
-        </li>
-
-        <li className="mb-10 ms-6">            
-            <span className="absolute flex items-center justify-center w-6 h-6 bg-white rounded-full -start-3 ring-8 ring-white">
-                <svg width="40px" height="40px" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
-                    <defs>
-                        <linearGradient x1="99.6865531%" y1="15.8007988%" x2="39.8359011%" y2="97.4378355%" id="linearGradient-1">
-                            <stop stopColor="#0052CC" offset="0%"></stop>
-                            <stop stopColor="#2684FF" offset="92.3%"></stop>
-                        </linearGradient>
-                    </defs>
-                    <g>
-                        <path d="M75.7929022,117.949352 C71.973435,113.86918 66.0220743,114.100451 63.4262382,119.292123 L0.791180865,244.565041 C-0.370000214,246.886207 -0.24632242,249.643151 1.11803323,251.85102 C2.48238888,254.058889 4.89280393,255.402741 7.48821365,255.402516 L94.716435,255.402516 C97.5716401,255.468706 100.19751,253.845601 101.414869,251.262074 C120.223468,212.37359 108.82814,153.245434 75.7929022,117.949352 Z" fill="url(#linearGradient-1)">
-
-                        </path>
-                        <path d="M121.756071,4.0114918 C86.7234975,59.5164098 89.0348008,120.989508 112.109989,167.141287 L154.170383,251.262074 C155.438703,253.798733 158.031349,255.401095 160.867416,255.401115 L248.094235,255.401115 C250.689645,255.401339 253.10006,254.057487 254.464416,251.849618 C255.828771,249.64175 255.952449,246.884805 254.791268,244.563639 C254.791268,244.563639 137.44462,9.83670492 134.492768,3.96383607 C131.853481,-1.29371311 125.14944,-1.36519672 121.756071,4.0114918 Z" fill="#2681FF">
-
-                        </path>
+            <li className="mb-10 ms-6">            
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-white rounded-full -start-3 ring-8 ring-white">
+                <svg className="fill-[#6fe3d0]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 228.35 105.4">
+                    {/* <defs>
+                    <style>.cls-1{fill:#6fe3d0;}
+                    </style>
+                    </defs> */}
+                    <g id="Layer_2" data-name="Layer 2">
+                    <g id="Layer_1-2" data-name="Layer 1">
+                        <path className="cls-1" d="M89.89,90a52.7,52.7,0,1,1,0-74.53l.13.12a9.16,9.16,0,0,1-.1,12.91h0l-30.7,30.7a9.2,9.2,0,0,1-13,0h0a9.2,9.2,0,0,1,0-13l23.29-23.3h0a34.29,34.29,0,1,0,7.37,54.1h0l61.57-61.51h0a52.7,52.7,0,1,1,0,74.52l-.06-.06a9.25,9.25,0,0,1,.09-13l30.65-30.64a9.2,9.2,0,0,1,13,0h0a9.2,9.2,0,0,1,0,13L158.84,82.56h0a34.29,34.29,0,1,0-7.38-54.1l0,0L89.9,90Z"/>
                     </g>
-                </svg>                
-            </span>
-            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900">Senior Software Engineer / SRE</h3>
-            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-black italic"><b>Atlassian Inc.</b> - Jan, 2019 - Jan 2024</time>
-            <div className="mb-4 text-base font-normal text-gray-500 content-between gap-y-4">
-                <ul className="list-disc">                    
-                <li className="pb-1.5">Led initiative for performance optimization of a widely used user-facing endpoint serving millions of customers,
-                    by algorithm redesign & SQL query optimisation, improving endpoint latency by 50% on average, eg 91s → 35s.
-                </li>
-                <li className="pb-1.5">
-                    Made multi-region databases in a globally distributed system, resilient to failovers, by optimising <b>Network TCP/IP settings</b> via <b>OS level programming</b>{" "}
-                    and optimising application database connection pool management in <b>JDBC Java Driver</b>.
-                </li>
-                <li className="pb-1.5">
-                    Delivered core features to an internal terraform provider in <b>Golang</b> using <b>HashiCorp SDK</b>.
-                </li>
-                <li className="pb-1.5">
-                    Developed a user interface (UI) used for monitoring, and management of deployments (history, deploying/re-deploying, error tracing), of synthetic checks, using <b>GraphQL</b>, <b>React Relay</b> and <b>TypeScript</b>, used by 50+ Engineering teams on a daily basis
-                </li>
-                <li className="pb-1.5">
-                    Improved scalability of message processing nodes by upgrading EC2 scaling policies, introduced node-based internal healthchecks and new metrics for cron-jobs. This reduced incident rate to 0% for background-processing
-                </li>
-                </ul>
-            </div>
-        </li>
+                    </g>
+                </svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900">Senior Full Stack Engineer<span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300 ms-3"><b>Latest</b></span></h3>
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-black italic"><b>Evermed</b> - May, 2024 - Present</time>
+                <div className="mb-4 text-base font-normal text-gray-500 content-between gap-y-4">
+                    <ul className="list-disc">                    
+                    <li className="pb-1.5">
+                        Rollout of new e-commerce web & back-end features, optimizing 
+                        existing features, reducing tech-debt and improving operations 
+                        in a fast-paced environment., with lots of pair-programming.
+                    </li>
+                    <li className="pb-1.5">
+                        Working with <b>AWS, Python, C#/.NET, CSS, Node.js, React.js, Nginx</b> for{" "}
+                        <i>full-stack application development</i>. Introduced test based approach 
+                        cloud-infra and front-end applications, outlining testing strategy
+                    </li>
+                    </ul>
+                </div>
+            </li>
+            <li className="mb-10 ms-6">            
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-white rounded-full -start-3 ring-8 ring-white">
+                    <svg width="40px" height="40px" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
+                        <defs>
+                            <linearGradient x1="99.6865531%" y1="15.8007988%" x2="39.8359011%" y2="97.4378355%" id="linearGradient-1">
+                                <stop stopColor="#0052CC" offset="0%"></stop>
+                                <stop stopColor="#2684FF" offset="92.3%"></stop>
+                            </linearGradient>
+                        </defs>
+                        <g>
+                            <path d="M75.7929022,117.949352 C71.973435,113.86918 66.0220743,114.100451 63.4262382,119.292123 L0.791180865,244.565041 C-0.370000214,246.886207 -0.24632242,249.643151 1.11803323,251.85102 C2.48238888,254.058889 4.89280393,255.402741 7.48821365,255.402516 L94.716435,255.402516 C97.5716401,255.468706 100.19751,253.845601 101.414869,251.262074 C120.223468,212.37359 108.82814,153.245434 75.7929022,117.949352 Z" fill="url(#linearGradient-1)">
 
+                            </path>
+                            <path d="M121.756071,4.0114918 C86.7234975,59.5164098 89.0348008,120.989508 112.109989,167.141287 L154.170383,251.262074 C155.438703,253.798733 158.031349,255.401095 160.867416,255.401115 L248.094235,255.401115 C250.689645,255.401339 253.10006,254.057487 254.464416,251.849618 C255.828771,249.64175 255.952449,246.884805 254.791268,244.563639 C254.791268,244.563639 137.44462,9.83670492 134.492768,3.96383607 C131.853481,-1.29371311 125.14944,-1.36519672 121.756071,4.0114918 Z" fill="#2681FF">
+
+                            </path>
+                        </g>
+                    </svg>                
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900">Senior Software Engineer / SRE</h3>
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-black italic"><b>Atlassian Inc.</b> - Jan, 2019 - Jan 2024</time>
+                <div className="mb-4 text-base font-normal text-gray-500 content-between gap-y-4">
+                    <ul className="list-disc">                    
+                    <li className="pb-1.5">Led initiative for performance optimization of a widely used user-facing endpoint serving millions of customers,
+                        by algorithm redesign & SQL query optimisation, improving endpoint latency by 50% on average, eg 91s → 35s.
+                    </li>
+                    <li className="pb-1.5">
+                        Made multi-region databases in a globally distributed system, resilient to failovers, by optimising <b>Network TCP/IP settings</b> via <b>OS level programming</b>{" "}
+                        and optimising application database connection pool management in <b>JDBC Java Driver</b>.
+                    </li>
+                    <li className="pb-1.5">
+                        Delivered core features to an internal terraform provider in <b>Golang</b> using <b>HashiCorp SDK</b>.
+                    </li>
+                    <li className="pb-1.5">
+                        Developed a user interface (UI) used for monitoring, and management of deployments (history, deploying/re-deploying, error tracing), of synthetic checks, using <b>GraphQL</b>, <b>React Relay</b> and <b>TypeScript</b>, used by 50+ Engineering teams on a daily basis
+                    </li>
+                    <li className="pb-1.5">
+                        Improved scalability of message processing nodes by upgrading EC2 scaling policies, introduced node-based internal healthchecks and new metrics for cron-jobs. This reduced incident rate to 0% for background-processing
+                    </li>
+                    </ul>
+                </div>
+            </li>
         </ol>
       </section>
 
