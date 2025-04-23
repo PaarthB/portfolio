81c5bc05d31e941f868749c2821bcb8071ba85ca
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-04-23 12:27:27 +1000

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
     return (
         <main>
             <Navbar />
-            <section className="p-20 bg-white pb-[3000px]">
+            <section className="p-20 bg-white pb-[300px]">
                 <h1 className="mb-8 text-2xl font-semibold tracking-tighter flex px-2">
                 Projects{" "}
                 </h1>
