45df7ecd1b15e580a2663a427cbd3b0266ba578c
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-09-13 07:17:52 +1000

    Update AboutMe.tsx

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -37,13 +37,13 @@ const AboutMe = forwardRef<HTMLDivElement, AboutMeProps>((props, ref) => {
 
             This unique background allows me to look at systems from a user’s perspective, always looking at what the user might like vs not so much, and how processes can be improved for developers.
 
-            <br/><br/>Recently, I have also gained experience in <span className="text-AAsecondary">geo-spatial intelligence tools</span> where I worked on a government funded project, doing lots of heavy lifting as a full stack developer from AI to databases to frontend to systems (like Martin Vector Tile Server). This was one of the projects where I learnt a lot from scratch.
+            <br/><br/>Recently, I have also recently gained experience in <span className="text-AAsecondary">geo-spatial intelligence tools</span> where I worked on a government funded project, as a full stack developer: from AI to databases to frontend to systems (like Martin Vector Tile Server). This was one of the projects where I to upskill and learn a lot from scratch.
 
             I have worked on multiple initiatives both on personal and professional projects, to improve <span className="text-AAsecondary">developer productivity</span>. I have a keen eye to design <span className="text-AAsecondary">sharp and performant user interfaces</span>, and also <span className="text-AAsecondary">backend apps</span>. I like to think about systems at scale, simplify them and also make them more <span className="text-AAsecondary">performant</span> and <span className="text-AAsecondary">reliable</span>.
 
             I like to sometimes dabble deep into the <span className="text-AAsecondary">Operating system</span>, troubleshooting network layer problems, which also helps me understand why systems behave the way they do.
 
-            <br/><br/>Even though my education being in engineering, initially limited me to tech focussed roles, me being who I am at heart, a true <span className="text-AAsecondary">analyst</span> and <span className="text-AAsecondary">problem solver</span>, ultimately made way for larger business focussed positions and tasks/roles, that come with a strong analytical and consulting focus whilst also being technical in nature.
+            <br/><br/>Even though my education being in engineering, initially limited me to tech focussed roles, me being who I am at heart, a true <span className="text-AAsecondary">analyst</span> and <span className="text-AAsecondary">problem solver</span>, has made way on many occasions for larger business focussed positions and tasks/roles, that come with a strong analytical and consulting focus whilst also being technical in nature.
 
             <br/><br/>This kind of self-driven motivation and leadership, has also been evident by my leadership in other domains, like culinary arts, curated leadership courses and surf life saving, and also being a tutor for UNSW, where I provided novel ideas
             for the betterment of the course to students.
