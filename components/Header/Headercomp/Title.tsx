78aeff457023080a42a7c5a4b1065238b9533034
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-09-19 12:45:11 +1000

    Add files via upload

---
+++ components/Header/Headercomp/Title.tsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion"
+
+const Title = (props: { rotate }) => {
+    return (     
+        <motion.div
+            animate={props.rotate ? { opacity: 0 } : { opacity: 1 }}
+            transition={{ opacity: { duration: 0 } }}
+            className="flex justify-end"
+        >
+            <img src="/img/title-md.jpg" alt="Paarth Bhasin" />
+            {/* <div className="w-6 h-0.5 rounded bg-AAsecondary"></div> */}
+        </motion.div>
+    )
+}
+
+export default Title;
+
