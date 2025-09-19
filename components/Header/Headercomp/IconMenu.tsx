780de4a9c7728234a84d5bf49650de6e309c4b21
Paarth Bhasin <paarthbhasin4@gmail.com>
2025-09-19 11:04:59 +1000

    Update IconMenu.tsx

--- components/Header/Headercomp/IconMenu.tsx
+++ components/Header/Headercomp/IconMenu.tsx
@@ -3,13 +3,13 @@ import { motion } from "../../../node_modules/framer-motion/dist/framer-motion";
 const IconMenu = (props: { rotate; setRotate; setShowElement; ShowElement,finishedLoading }) => {
   return (
     <div
-      className="md:hidden  text-white space-y-2 left-0 hover:cursor-pointer mt-2"
-      onClick={() => {
-        props.setRotate(!props.rotate);
-        props.setShowElement(!props.ShowElement);
-      }}
+      className="text-white space-y-2 left-0 hover:cursor-pointer mt-2"
+      // onClick={() => {
+      //   props.setRotate(!props.rotate);
+      //   props.setShowElement(!props.ShowElement);
+      // }}
     >
-      <div className="flex justify-end">
+      {/* <div className="flex justify-end">
         <motion.div
           animate={props.rotate ? { y: 10, rotate: 45 } : { rotate: 0, y: 0 }}
           transition={
@@ -17,15 +17,16 @@ const IconMenu = (props: { rotate; setRotate; setShowElement; ShowElement,finish
           }
           className="w-8 h-0.5 rounded bg-AAsecondary"
         ></motion.div>
-      </div>
+      </div> */}
       <motion.div
         animate={props.rotate ? { opacity: 0 } : { opacity: 1 }}
         transition={{ opacity: { duration: 0 } }}
         className="flex justify-end"
       >
-        <div className="w-6 h-0.5 rounded bg-AAsecondary"></div>
+        <img src="/img/title-md.jpg" alt="Paarth Bhasin" fetchpriority="high" />
+        {/* <div className="w-6 h-0.5 rounded bg-AAsecondary"></div> */}
       </motion.div>
-      <div className="flex justify-end">
+      {/* <div className="flex justify-end">
         <motion.div
           animate={props.rotate ? { y: -10, width: "150%", rotate: -45 } : { y: 0, rotate: 0, width: "50%" }}
           transition={
@@ -33,7 +34,7 @@ const IconMenu = (props: { rotate; setRotate; setShowElement; ShowElement,finish
           }
           className="w-4 h-0.5 rounded bg-AAsecondary"
         ></motion.div>
-      </div>
+      </div> */}
     </div>
   );
 };
