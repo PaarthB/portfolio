637bc901385b3cc2ea3dd630cc75cd849fd8599f
Paarth Bhasin <paarthbhasin4@gmail.com>
2026-05-06 07:38:37 +1000

    fix logo on nvabar

--- components/Header/Headercomp/Logo.tsx
+++ components/Header/Headercomp/Logo.tsx
@@ -2,31 +2,18 @@ import React from "react";
 import { motion } from "../../../node_modules/framer-motion/dist/framer-motion";
 export default function Logo(props: { finishedLoading: boolean }) {
   return (
-    <>
-      <motion.div
-        initial={{ y: 10, opacity: 0 }}
-        animate={{ y: 10, opacity: 1 }}
-        transition={{
-          type: "spring",
-          y: { delay: props.finishedLoading ? 0 : 8, duration: 0 },
-          opacity: { delay: props.finishedLoading ? 0 : 8, duration: 0 },
-        }}
-        className=" relative h-12 w-10 "
-      >
-        <motion.span
-          initial={{ x: 1 }}
-          className="absolute h-full w-full flex justify-center items-center text-lg font-Header text-AAsecondary"
-        >
-          P.B
-        </motion.span>
-
-        <motion.div animate={{ rotate: -30, y: 5 }} className="h-1 w-6 rounded bg-AAsecondary"></motion.div>
-        <motion.div animate={{ rotate: 90, x: -10, y: 18 }} className="h-1 w-6 rounded bg-AAsecondary"></motion.div>
-        <motion.div animate={{ rotate: 30, y: 31 }} className="h-1 w-6 rounded bg-AAsecondary"></motion.div>
-        <motion.div animate={{ rotate: -30, y: 27, x: 19 }} className="h-1 w-6 rounded bg-AAsecondary"></motion.div>
-        <motion.div animate={{ rotate: 30, x: 19, y: -10 }} className="h-1 w-6 rounded bg-AAsecondary"></motion.div>
-        <motion.div animate={{ rotate: 90, x: 28, y: 2 }} className="h-1 w-6 rounded bg-AAsecondary"></motion.div>
-      </motion.div>
-    </>
+    <div className="flex items-center justify-center space-x-3">
+      <img src="/favicon.ico" className="h-16 w-16 object-contain" alt="Logo" />
+      {/* <img
+        src="/img/title-md-extra-bold-no-bg.png"
+        className="h-10 w-auto object-contain hidden sm:block"
+        alt="Title"
+      />
+      <img
+        src="/img/title-md-extra-bold-no-bg.png"
+        className="h-8 w-auto object-contain sm:hidden"
+        alt="Title"
+      /> */}
+    </div>
   );
 }
